3f8b1e4a3b74efceb6622b4e50cb118752efada5
yyx319 <44246875+yyx319@users.noreply.github.com>
2022-02-02 12:52:06 +0000

    Add files via upload

---
+++ numerical_util.py
@@ -0,0 +1,16 @@
+def rebin(a, *args):
+  '''rebin ndarray data into a smaller ndarray of the same rank whose dimensions
+    are factors of the original dimensions. eg. An array with 6 columns and 4 rows
+    can be reduced to have 6,3,2 or 1 columns and 4,2 or 1 rows.
+    example usages:
+    >>> a=rand(6,4); b=rebin(a,3,2)
+    >>> a=rand(6); b=rebin(a,2)
+    '''
+    shape = a.shape
+    lenShape = len(shape)
+    factor = np.array(np.asarray(shape)/np.asarray(args), dtype=int)
+    evList = ['a.reshape('] + \
+             ['args[%d],factor[%d],' % (i, i) for i in range(lenShape)] + \
+             [')'] + ['.mean(%d)' % (i+1) for i in range(lenShape)]
+    print(''.join(evList))
+    return eval(''.join(evList))
